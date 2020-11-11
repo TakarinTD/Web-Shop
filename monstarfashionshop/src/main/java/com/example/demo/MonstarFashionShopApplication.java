410c93cb6164c72215d7e70445bf424216fac158
TakarinTD <56709998+TakarinTD@users.noreply.github.com>
2020-11-11 14:02:28 +0700

    Giao diện

--- monstarfashionshop/src/main/java/com/example/demo/MonstarFashionShopApplication.java
+++ monstarfashionshop/src/main/java/com/example/demo/MonstarFashionShopApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class MonstarFashionShopApplication {
 
 	public static void main(String[] args) {
