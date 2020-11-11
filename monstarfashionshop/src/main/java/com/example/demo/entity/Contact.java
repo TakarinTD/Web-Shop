f63db9c114ca8626b3696badbb819478e978d45b
nguyenphamvan <nguyenphamvan1998@gmail.com>
2020-11-11 11:12:58 +0700

    change structure database and add source code with entity and repository

---
+++ monstarfashionshop/src/main/java/com/example/demo/entity/Contact.java
@@ -0,0 +1,67 @@
+package com.example.demo.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "contact")
+public class Contact {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "guest_name")
+    private String guestName;
+
+    @Column(name = "email")
+    private String emal;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @Column(name = "message")
+    private String message;
+
+    public Contact() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public void setGuestName(String guestName) {
+        this.guestName = guestName;
+    }
+
+    public String getEmal() {
+        return emal;
+    }
+
+    public void setEmal(String emal) {
+        this.emal = emal;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
---
+++ monstarfashionshop/src/main/java/com/example/demo/entity/ProductSize.java
@@ -0,0 +1,81 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "product_size")
+public class ProductSize {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "size_name")
+    private String sizeName;
+
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    @OneToMany(mappedBy = "productSize", fetch = FetchType.LAZY)
+    private Set<ProductDetail> productDetails;
+
+    public ProductSize() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSizeName() {
+        return sizeName;
+    }
+
+    public void setSizeName(String sizeName) {
+        this.sizeName = sizeName;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Set<ProductDetail> getProductDetails() {
+        return productDetails;
+    }
+
+    public void setProductDetails(Set<ProductDetail> productDetails) {
+        this.productDetails = productDetails;
+    }
+
+    public void addProductDetail(ProductDetail productDetail) {
+        if (productDetails == null) {
+            productDetails = new HashSet<>();
+        }
+        productDetails.add(productDetail);
+    }
+}
