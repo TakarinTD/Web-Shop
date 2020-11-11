b7c887d130a2ecee35d2b772167256acde04493c
TakarinTD <56709998+TakarinTD@users.noreply.github.com>
2020-11-11 14:35:03 +0700

    Fix

--- monstarfashionshop/src/main/java/com/example/demo/MonstarfashionshopApplication.java
+++ monstarfashionshop/src/main/java/com/example/demo/MonstarfashionshopApplication.java
@@ -2,13 +2,12 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.*;
 
 @SpringBootApplication
-public class MonstarFashionShopApplication {
+public class MonstarfashionshopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MonstarFashionShopApplication.class, args);
+		SpringApplication.run(MonstarfashionshopApplication.class, args);
 	}
 
 }
