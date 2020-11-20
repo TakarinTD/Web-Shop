1a8c263457030317992aa758c46f812bca179d59
Nguyễn Duy Hoài Lâm <56709998+TakarinTD@users.noreply.github.com>
2020-11-20 07:44:55 +0700

    Revert "Nguyen pham"

--- monstarfashionshop/src/main/java/com/example/demo/MonstarfashionshopApplication.java
+++ monstarfashionshop/src/main/java/com/example/demo/MonstarfashionshopApplication.java
@@ -1,13 +1,23 @@
 package com.example.demo;
 
+import com.example.demo.service.*;
+import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.example.demo")
-public class MonstarfashionshopApplication{
+@SpringBootApplication (scanBasePackages = "com.example.demo")
+public class MonstarfashionshopApplication
+//        implements CommandLineRunner
+{
 
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         SpringApplication.run(MonstarfashionshopApplication.class, args);
     }
+    @Autowired
+    private ProductDetailService productDetailService;
 
+//    @Override
+//    public void run (String... args) throws Exception {
+//        System.out.println(productDetailService.findQuantity((long)15, (long)1, (long)1));
+//    }
 }
--- monstarfashionshop/src/main/java/com/example/demo/service/ProductService.java
+++ monstarfashionshop/src/main/java/com/example/demo/service/ProductService.java
@@ -2,38 +2,23 @@ package com.example.demo.service;
 
 import com.example.demo.dto.ProductDTO;
 import com.example.demo.entity.Product;
-import com.example.demo.entity.ProductColor;
-import com.example.demo.entity.ProductDetail;
-import com.example.demo.entity.ProductSize;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
+import java.util.*;
+import org.springframework.data.domain.*;
 
-import javax.transaction.Transactional;
-import java.util.List;
-import java.util.Map;
-
-@Service
-@Transactional
 public interface ProductService {
 
-    ProductDTO convertProductToProductDTO(Product product);
+    ProductDTO convertProductToProductDTO (Product product);
 
-    List<ProductDTO> getProductsByCategoryId(long categoryId);
+    List<ProductDTO> getProductsByCategoryId (long categoryId);
 
-    List<ProductDTO> getProductsTopSaleMan();
+    List<Product> findProducts (String categoryName);
 
-    List<ProductDTO> getProductsTopSaleWomen();
+    Optional<Product> findProduct (Long id);
 
-    Page<Product> pageProductByCategoryId(Long categoryId, int pageNum, int pageSize, String sortField, String sortDir);
+    Page<Product> pageProductByCategoryId (Long categoryId, int pageNum, int pageSize);
 
-    public List<Product> findProducts(String categoryName);
+    List<ProductDTO> getProductsTopSaleMan ();
 
-    Page<Product> findProductByKeyword(String keyword, int page, int pageSize, String sortField, String sortDir);
+    List<ProductDTO> getProductsTopSaleWomen ();
 
-    Page<Product> findProductsWithCondition(Long categoryId, List<Long> colorIds, List<Long> sizeIds, float price1, float price2, int page, int pageSize, String sortField, String sortDir);
-
-    Map<ProductColor, Integer> getListColorAndAmount(Long categoryId);
-
-    Map<ProductSize, Integer> getListSizeAndAmount(Long categoryId);
 }
