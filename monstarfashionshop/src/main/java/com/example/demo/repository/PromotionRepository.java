a1878977e3b1c1bec1759aab68dd086342da2411
nguyenphamvan <nguyenphamvan1998@gmail.com>
2020-11-23 21:04:33 +0700

    delete resource has errors

--- monstarfashionshop/src/main/java/com/example/demo/repository/PromotionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.Promotion;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PromotionRepository extends JpaRepository<Promotion, Long> {
-}
--- monstarfashionshop/src/main/java/com/example/demo/service/ProductDetailService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.entity.*;
-import java.util.*;
-
-public interface ProductDetailService {
-    List<ProductDetail> findProductDetails(Long productId);
-    int findQuantity(Long productId, Long colorId, Long sizeId);
-}
